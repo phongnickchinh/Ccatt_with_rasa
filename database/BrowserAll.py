3ca987d8ebe3a4a2d6e49a61b1e344c5e2877532
phonggiotai <114666846+phonggiotai@users.noreply.github.com>
2024-03-24 11:37:12 +0700

    add file log to debug, success rate increate to 38%, will up to be close 100% in next commit

--- database/BrowserAll.py
+++ database/BrowserAll.py
@@ -1,55 +1,90 @@
 from ClearContent import try_pull
 
 #Thực hiện duyệt qua danh sách listTour-trongNuoc.txt
-
 # Path: database/BrowserAll.py
-
 with open("database/listTour-trongNuoc.txt", "r") as f:
     listTour = f.read().split("\n")
 
 #duyệt qua từng tour trong listTour
 #tạo các biến để lưu lỗi và số lượng tour đã pull thành công
+i, success = 0, 0
+error_in_get_basic_info, error_in_get_highlights, error_in_get_tour_detail = 0, 0, 0
+error_in_get_price_on_departure, error_in_get_price_detail, error_in_get_policy = 0, 0, 0
+#Clean các log trước đó
+with open("database/log_success.txt", "w") as f:
+    f.write("")
+with open("database/log_fail.txt", "w") as f:
+    f.write("")
 
-i=0
-success=0
-error_in_get_basic_info=0
-error_in_get_highlights=0
-error_in_get_tour_detail=0
-error_in_get_price_on_departure=0
-error_in_get_price_detail=0
-error_in_get_policy=0
-
+#duyệt qua từng tour trong listTour
 for tour in listTour:
-    print("Tour" +str(i+1) + ": " + tour)
-    result = try_pull(tour)
-    if result=="success":
-        success+=1
-        print("Success" + str(success))
-    else:
+    with open("database/log_success.txt", "a", encoding="utf-8") as f:
         i+=1
-        print( result[0])
-        if "get_basic_info" in result[1]:
-            error_in_get_basic_info+=1
-        if "get_highlights" in result[1]:
-            error_in_get_highlights+=1
-        if "get_tour_detail" in result[1]:
-            error_in_get_tour_detail+=1
-        if "get_price_on_departure" in result[1]:
-            error_in_get_price_on_departure+=1
-        if "get_price_detail" in result[1]:
-            error_in_get_price_detail+=1
-        if "get_policy" in result[1]:
-            error_in_get_policy+=1
+        check,list_result = try_pull(tour)
+        if check=="success":
+            success+=1
+            #ghi lại list_result của tour thành công vào file log_success
+            f.write("Tour "+str(i)+": "+tour+"\n")
+            f.write("Title: "+str(list_result[0])+"\n")
+            f.write("Location: "+str(list_result[1])+"\n")
+            f.write("Departure Place: "+str(list_result[2])+"\n")
+            f.write("Length: "+str(list_result[3])+"\n")
+            f.write("Vehicle: "+str(list_result[4])+"\n")
+            f.write("Tour ID: "+str(list_result[5])+"\n")
+            f.write("List Extra Service: "+str(list_result[6])+"\n")
+            f.write("Highlights: "+str(list_result[7])+"\n")
+            f.write("Detail: "+str(list_result[8])+"\n")
+            f.write("Price on Departure: "+str(list_result[9])+"\n")
+            f.write("Price Detail: "+str(list_result[10])+"\n")
+            f.write("Note: "+str(list_result[11])+"\n")
+            f.write("Policy Note: "+str(list_result[12])+"\n")
+            f.write("Policy Register: "+str(list_result[13])+"\n")
+            f.write("Policy Register Note: "+str(list_result[14])+"\n")
+            f.write("\n")
+            f.write("------------------------------------------------------------------------------\n")
+            
+        if check=="mini fail":
+            #ghi lại list_result của tour không thành công vào file log_fail
+            with open("database/log_fail.txt", "a", encoding="utf-8") as f:
+                f.write("Tour "+str(i)+": "+tour+"\n")
+                f.write("Error in: "+str(list_result)+"\n")
+                f.write("\n")
+            if "get_basic_info" in list_result:
+                error_in_get_basic_info+=1
+            if "get_highlights" in list_result:
+                error_in_get_highlights+=1
+            if "get_tour_detail" in list_result:
+                error_in_get_tour_detail+=1
+            if "get_price_on_departure" in list_result:
+                error_in_get_price_on_departure+=1
+            if "get_price_detail" in list_result:
+                error_in_get_price_detail+=1
+            if "get_policy" in list_result:
+                error_in_get_policy+=1
+        if check=="fail":
+            with open("database/log_fail.txt", "a", encoding="utf-8") as f:
+                f.write("Tour "+str(i)+": "+tour+"\n")
+                f.write("Error in: pull_and_clean\n")
+                f.write("---------------------------------------------------------\n")
 
-    print("\n-----------------------------------------------------------------------------\n")
+print("Success: ", success)
+print("Error in get_basic_info: ", error_in_get_basic_info)
+print("Error in get_highlights: ", error_in_get_highlights)
+print("Error in get_tour_detail: ", error_in_get_tour_detail)
+print("Error in get_price_on_departure: ", error_in_get_price_on_departure)
+print("Error in get_price_detail: ", error_in_get_price_detail)
+print("Error in get_policy: ", error_in_get_policy)
 
-print("Tổng số tour đã pull thành công: " + str(success))
-print("Số lỗi trong hàm get_basic_info: " + str(error_in_get_basic_info))
-print("Số lỗi trong hàm get_highlights: " + str(error_in_get_highlights))
-print("Số lỗi trong hàm get_tour_detail: " + str(error_in_get_tour_detail))
-print("Số lỗi trong hàm get_price_on_departure: " + str(error_in_get_price_on_departure))
-print("Số lỗi trong hàm get_price_detail: " + str(error_in_get_price_detail))
-print("Số lỗi trong hàm get_policy: " + str(error_in_get_policy))
+with open("database/log_success.txt", "a") as f:
+    f.write("Success: "+str(success)+"\n")
+with open("database/log_fail.txt", "a", encoding="utf-8") as f:
+    f.write("Error in get_basic_info: "+str(error_in_get_basic_info)+"\n")
+    f.write("Error in get_highlights: "+str(error_in_get_highlights)+"\n")
+    f.write("Error in get_tour_detail: "+str(error_in_get_tour_detail)+"\n")
+    f.write("Error in get_price_on_departure: "+str(error_in_get_price_on_departure)+"\n")
+    f.write("Error in get_price_detail: "+str(error_in_get_price_detail)+"\n")
+    f.write("Error in get_policy: "+str(error_in_get_policy)+"\n")
+    f.write("\n")
 
 
     
--- database/ClearContent.py
+++ database/ClearContent.py
@@ -26,7 +26,7 @@ def get_basic_info(div, bigDiv):
 
         return departure_place, length, vehicle, tour_id, list_extra_service
     except Exception as e:
-        #print("Error in basic_info: " + str(e))
+        print("Error in basic_info: " + str(e))
         return None, None, None, None, None
 
 #lấy điểm nhấn hành trình
@@ -34,14 +34,18 @@ def get_highlights(div):
     try:
         highlights=[]
         single_box_excerpt = div.find("div", class_="single-box-excerpt")
-        for li in single_box_excerpt.find_all("li"):
-            highlights.append(li.text.strip().replace("\xa0"," "))
-        print(highlights)
+        if single_box_excerpt.find("li")==None:
+            highlights.append(single_box_excerpt.text.strip().replace("\xa0"," "))
+        else:
+            for li in single_box_excerpt.find_all("li"):
+                highlights.append(li.text.strip().replace("\xa0"," "))
+
         if len(highlights)==0:
             return None
         return highlights
+    
     except Exception as e:
-        #print("Error in highlights: " + str(e))
+        print("Error in highlights: " + str(e))
         return None
 
 #lấy thông tin chi tiét tour
@@ -52,7 +56,7 @@ def get_tour_detail(div):
         array = array.split("LƯU Ý")[0]
         return  array
     except Exception as e:
-        #print("Error in tour_detail: " + str(e))
+        print("Error in tour_detail: " + str(e))
         return None
 
 #lấy bảng giá mỗi điểm khởi hành
@@ -60,26 +64,44 @@ def get_price_on_departure(div):
     try:
         array=[]
         table=div.find("table", class_="banggia")
+        if table==None:
+            return "No price_table found"
         #lâsy bảng này lưu vào mảng 2 chiều
         for tr in table.find_all("tr"):
             row=[]
             for td in tr.find_all("td"):
-                row.append(td.text.strip().replace("\n","").replace("\t","").replace("\r",""))
+                row.append(td.text.strip().replace("\n","").replace("\t","").replace("\r","").replace("\xa0"," ").replace("  "," "))
             array.append(row)
         return array
     except Exception as e:
-        #print("Error in get_price_on_departure: " + str(e))
+        print("Error in get_price_on_departure: " + str(e))
         return None
     
 #lấy các chi tiết bảng giá
 def get_price_detail(div):
     try:
-        array=div.find("div", id="table-price-1").find_all("ul")
-        #mỗi ul lấy từng li luu vào mảng
-        array = [[li.text.replace("\r", "").replace("\t"," ").replace("\n","").strip().replace("\xa0"," ") for li in ul.find_all("li")] for ul in array]
-        noteArray = div.find("div", id="table-price-1").find(lambda tag: tag.name == 'p' and tag.text.strip() == 'Lưu ý:').findNextSibling().findChildren()
-        noteArray = [x.text.replace("\r", "").replace("\t"," ").replace("\n","") for x in noteArray if x.text != ""]
-        return array, noteArray
+        array=div.find("div", id="table-price-1").find_all(["ul","ol"])
+        if len(array)==0:
+            array=None
+        else:
+            for i in range(len(array)):
+                list_li=array[i].find_all("li")
+                array[i]=[x.text.replace("\r", "").replace("\t"," ").replace("\n","").replace("\xa0", " ").strip() for x in list_li if x.text != ""]
+
+
+        noteArray = None
+        #trong trường hợp từ lưu ý được ghi riêng ở thẻ p, sau đó là đến thẻ chứa nội dung lưu ý thì sử dụng findnextsibling
+        #nếu từ lưu ý và note được ghi chung ở trong 1 thẻ p thì lấy luôn thẻ đó
+        try:
+            if(div.find("div", id="table-price-1").find(lambda tag: tag.name == 'p' and tag.text.strip() == 'Lưu ý:')!=None):
+                noteArray = div.find("div", id="table-price-1").find(lambda tag: tag.name == 'p' and tag.text.strip() == 'Lưu ý:').findNextSibling().findChildren()
+                noteArray = [x.text.replace("\r", "").replace("\t"," ").replace("\n","") for x in noteArray if x.text != ""]
+            if(div.find("div", id="table-price-1").find(lambda tag: tag.name == 'p' and tag.text.strip() == 'Lưu ý:')==None):
+                noteArray = div.find("div", id="table-price-1").find(lambda tag: tag.name == 'p' and 'Lưu ý' in tag.text.strip()).text.strip()
+            return array, noteArray
+        except Exception as e:
+            noteArray="No note_list found"
+            return array, noteArray
     except Exception as e:
         #print("Error in get_price_detail: " + str(e))
         return None, None
@@ -89,10 +111,12 @@ def get_policy(div):
     try:
         tour_rules_tag=div.find("div", id="tour-rule-2")
         #quy trình đăng lý tour
-        policy_register=tour_rules_tag.find(lambda tag: tag.name=='h3' and tag.text.strip()=='QUY TRÌNH ĐĂNG KÝ TOUR').findNextSibling().text.strip()
+        policy_register=tour_rules_tag.find(lambda tag: tag.name=='h3' and tag.text.strip()=='QUY TRÌNH ĐĂNG KÝ TOUR').findNextSibling().text.strip().replace("\r", "").replace("\t"," ").replace("\n","")
         policy_register_note=tour_rules_tag.find(lambda tag: tag.name=='h3' and tag.text.strip()=='QUY TRÌNH ĐĂNG KÝ TOUR').findNextSibling().findNextSibling().text.strip()
         #tìm kiếm thông tin LÀ THẺ ĐẰNG SAU thẻ h3 vào nôi dung chưa đoạn NHỮNG LƯU Ý KHÁC
-        policy_note=div.find("div", id="tour-rule-2").find(lambda tag: tag.name=='h3' and tag.text.strip()=='NHỮNG LƯU Ý KHÁC').findNextSibling().text.strip()
+        policy_note_tag=div.find("div", id="tour-rule-2").find(lambda tag: tag.name=='h3' and tag.text.strip()=='NHỮNG LƯU Ý KHÁC').findNextSibling().findChildren()
+        policy_note=[x.text.strip().replace("\r", "").replace("\t"," ").replace("\n","") for x in policy_note_tag if x.text != ""]
+
         return policy_note, policy_register, policy_register_note
     except Exception as e:
         #print("policy not clear:"+str(e))
@@ -117,11 +141,6 @@ def pull_and_clean(url):
 
     #lấy thông tin cơ bản
     departure_place, length, vehicle, tour_id, list_extra_service = get_basic_info(div,soup)
-    # print(departure_place)
-    # print(length)
-    # print(vehicle)
-    # print(tour_id)
-    # print(list_extra_service)
 
     #điểm nhấn hành trình
     highlights = get_highlights(div)
@@ -168,34 +187,35 @@ def try_pull(url):
 
         #kiểm tra xem hàm nào trả về None, thêm vào list return tên hàm đó
         list_return=[]
+        list_tour_info=[title, location, departure_place, length, vehicle, tour_id, list_extra_service, highlights, detail, price_on_departure, price_detail, note, policy_note, policy_register, policy_register_note]
+        
+        #ghi log lỗi mỗi hàm
         if title==None or location==None or departure_place==None or length==None or vehicle==None or tour_id==None or list_extra_service==None:
             list_return.append("get_basic_info")
-
         if highlights==None:
             list_return.append("get_highlights")
-            
         if detail==None:
             list_return.append("get_tour_detail")
-
         if price_on_departure==None:
             list_return.append("get_price_on_departure")
-
         if price_detail==None or note==None:
             list_return.append("get_price_detail")
-
         if policy_note==None or policy_register==None or policy_register_note==None:
             list_return.append("get_policy")
 
         if len(list_return)==0:
-            return "success"
+            return "success",list_tour_info
         else:
             return "mini fail", list_return
 
     except Exception as e:
         print(e)
-        return "fail"
+        return "fail",None
 
-url_P="https://www.vietnambooking.com/du-lich/tour-nha-trang-dao-yen-3n3d.html"
+
+
+url_P="https://www.vietnambooking.com/du-lich/tour-cao-bang-bac-kan-lang-son-3n2d.html"
+print(url_P)
 try_pull(url_P)
 
 
